9cc91a6ee745a7335d043359832a99efd9d26b1f
Peter Davidson <pdavidso@gmail.com>
2014-11-23 12:42:57 -0700

    Hooked it up asynch using servlet 3.0

--- src/main/java/us/pdavidson/jersey/MyResource.java
+++ src/main/java/us/pdavidson/jersey/MyResource.java
@@ -1,5 +1,6 @@
 package us.pdavidson.jersey;
 
+import rx.functions.Action1;
 import us.pdavidson.jersey.spring.MySpringBean;
 
 import javax.inject.Inject;
@@ -7,6 +8,9 @@ import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import java.util.List;
 
 @Path("/")
 @Singleton
@@ -21,6 +25,20 @@ public class MyResource {
     @GET
     @Path("{name}")
     public String hello(@PathParam("name")String name){
-        return springBean.sayHello(name);
+        String first = springBean.friends().toBlocking().first();
+        return first;
     }
+
+    @GET
+    @Path("{name}/friends")
+    public void asynch(@Suspended final AsyncResponse asyncResponse){
+        springBean.friends().buffer(3).subscribe(new Action1<List<String>>() {
+            @Override
+            public void call(List<String> s) {
+                System.out.println("s = [" + s + "]");
+                asyncResponse.resume(s.toString());
+            }
+        });
+    }
+
 }
--- src/main/java/us/pdavidson/jersey/spring/MySpringBean.java
+++ src/main/java/us/pdavidson/jersey/spring/MySpringBean.java
@@ -1,8 +1,15 @@
 package us.pdavidson.jersey.spring;
 
+import rx.Observable;
+
 public class MySpringBean {
+    private static final String [] myFriends = {"Tina", "Napoleon", "Pedro"};
 
     public String sayHello(String name){
         return "What Does The Fox say " + name;
     }
+
+    public Observable<String> friends(){
+        return Observable.from(myFriends);
+    };
 }
